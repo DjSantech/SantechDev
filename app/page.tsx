069ace8248eba2167f86a3cf02d5aab3679c7dc8
santiago guevara mendez <santiago.guevara1@utp.edu.co>
2024-12-10 15:09:12 -0500

    Primer paso de mi portafolio

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import CoverParticles from "../components/cover-particles";
+
+export default function Home() {
+  return (
+    <main>
+      <div className="flex min-h-[100vh] h-full bg-no-repeat bg-gradient-cover"></div>
+      <CoverParticles/>
+      <p>Introducción</p>
+    </main>
+  );
+}
