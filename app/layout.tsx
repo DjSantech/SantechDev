42f860edc2c5af3f1f0a92f216cc2361d2294472
santiago guevara mendez <santiago.guevara1@utp.edu.co>
2025-01-20 15:33:36 -0500

    solucion a error de despliegue segundo intento

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,17 @@ import type { Metadata } from "next";
 import { Urbanist } from "next/font/google";
 
 import "./globals.css";
+import dynamic from 'next/dynamic';
 import 'swiper/css';
+import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
+
+const Swiper = dynamic(() => import('swiper/react').then((mod) => mod.Swiper), {
+  ssr: false,
+});
+const SwiperSlide = dynamic(() => import('swiper/react').then((mod) => mod.SwiperSlide), {
+  ssr: false,
+});
 
 import Navbar from "@/components/navbar";
 import Header from "@/components/header";
