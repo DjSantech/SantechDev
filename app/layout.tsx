00ac1aeb44d8f250cfaca93b0eda1ab1d39ba7af
santiago guevara mendez <santiago.guevara1@utp.edu.co>
2025-01-20 16:17:52 -0500

    despliegue exitoso

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,8 +12,8 @@ import Header from "@/components/header";
 const urbanist = Urbanist({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "TarreDev Landing Page",
-  description: "Landing page made by TarreDev",
+  title: "SantechDev Landing Page",
+  description: "Landing page made by SantechDev",
 };
 
 export default function RootLayout({
