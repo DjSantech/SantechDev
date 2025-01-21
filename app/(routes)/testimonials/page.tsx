bc2a3544799c4898d7c3b543425febf65b9d912f
santiago guevara mendez <santiago.guevara1@utp.edu.co>
2025-01-21 12:48:40 -0500

    apartado de portfolio terminado

--- app/(routes)/testimonials/page.tsx
+++ app/(routes)/testimonials/page.tsx
@@ -16,8 +16,8 @@ const TestimonialsPage = () => {
             <div className='flex flex-col justify-center h-lvh'>
                 <CircleImage />
                 <h1 className="text-2xl leading-tight text-center md:text-4xl md:mb-5">
-                    Algunos comentarios
-                    <span className="block font-bold text-secondary"> de nuestros clientes</span>
+                Some comments 
+                    <span className="block font-bold text-secondary"> from our customers</span>
                 </h1>
                 <div className="flex items-center justify-center">
                     <div>
--- data.tsx
+++ data.tsx
@@ -154,60 +154,19 @@ export const serviceData = [
 export const dataPortfolio = [
     {
         id: 1,
-        title: "Web Pro",
-        image: "/image-1.jpg",
-        urlGithub: "#!",
-        urlDemo: "#!",
+        title: "Cercafe Memorama",
+        image: "/cercafe-memorama.png",
+        urlGithub: "https://github.com/DjSantech/CercafeMemorama2",
+        urlDemo: "https://cercafe-memorama2.vercel.app",
     },
     {
         id: 2,
-        title: "Desarrollo Web Ágil",
-        image: "/image-2.jpg",
-        urlGithub: "#!",
-        urlDemo: "#!",
+        title: "TurboCars Database",
+        image: "/turbocars-database.png",
+        urlGithub: "https://github.com/DjSantech/TurboCarsDatabase",
+        urlDemo: "https://turbo-cars-database.vercel.app",
     },
-    {
-        id: 3,
-        title: "Estrategias Web",
-        image: "/image-3.jpg",
-        urlGithub: "#!",
-        urlDemo: "#!",
-    },
-    {
-        id: 4,
-        title: "Ideas Creativas",
-        image: "/image-4.jpg",
-        urlGithub: "#!",
-        urlDemo: "#!",
-    },
-    {
-        id: 5,
-        title: "Webs Impactantes",
-        image: "/image-5.jpg",
-        urlGithub: "#!",
-        urlDemo: "#!",
-    },
-    {
-        id: 6,
-        title: "Web Dinámica",
-        image: "/image-6.jpg",
-        urlGithub: "#!",
-        urlDemo: "#!",
-    },
-    {
-        id: 7,
-        title: "Dark Web ",
-        image: "/image-7.jpg",
-        urlGithub: "#!",
-        urlDemo: "#!",
-    },
-    {
-        id: 8,
-        title: "E-commerce web",
-        image: "/image-8.jpg",
-        urlGithub: "#!",
-        urlDemo: "#!",
-    }
+    
 ];
 
 export const dataTestimonials = [
